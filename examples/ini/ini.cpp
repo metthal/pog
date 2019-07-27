d11979ec0dc8fb6395c3ee1ea4c486d48b0e4b90
Marek Milkovič <milkovic.marek@gmail.com>
2019-07-27 22:16:58 +0200

    Fixed examples build

--- examples/ini/ini.cpp
+++ examples/ini/ini.cpp
@@ -7,9 +7,6 @@
 
 using namespace pog;
 
-template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
-
 struct NoData{};
 using Value = std::variant<NoData, bool, int, double, std::string>;
 
