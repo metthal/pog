0099396445d892bd4df57c00c38c92b74abc2945
Tadeas Kucera <tadeas.kucera@avast.com>
2019-09-26 14:02:08 +0200

    Simplify the regexp example

--- examples/regexp/regexp.cpp
+++ examples/regexp/regexp.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 
 #include <pog/parser.h>
+#include <pog/html_report.h>
 
 using namespace pog;
 
@@ -89,54 +90,54 @@ int main()
 	_parser.set_start_symbol("regexp");
 
 	_parser.rule("regexp") //shared_ptr<String>
-		.production("SLASH", "regexp_body", "SLASH", [&](auto&& args) -> Value {
+		.production("SLASH", "regexp_concat", "SLASH", [&](auto&& args) -> Value {
 			return std::move(args[1]);
 		})
 		;
-	_parser.rule("regexp_body") //shared_ptr<String>
-		.production("regexp_or", [&](auto&& args) -> Value {
-			return std::move(args[0]);
-		});
+	//// _parser.rule("regexp_body") //shared_ptr<String>
+	//// 	.production("regexp_or", [&](auto&& args) -> Value {
+	//// 		return std::move(args[0]);
+	//// 	});
 
-	_parser.rule("regexp_or") //shared_ptr<RegexpUnit>
-		.production("regexp_concat", [](auto&& args) -> Value { std::cout << "concat init" << std::endl; return std::move(args[0]); })
-		.production("regexp_or", "REGEXP_OR", "regexp_concat", [](auto&& args) -> Value {
-			// std::cout << "concat composed" << std::endl;
-			std::string arg = std::move(args[0].getString());
-			std::vector<std::string> concat = std::move(args[2].getVector());
-			concat.push_back(std::move(arg));
-			return Value(std::move(concat));
-		})
+	//// _parser.rule("regexp_or") //shared_ptr<RegexpUnit>
+		//// .production("regexp_concat", [](auto&& args) -> Value { return std::move(args[0]); })
+		//// .production("regexp_or", "REGEXP_OR", "regexp_concat", [](auto&& args) -> Value {
+		//// 	// std::cout << "concat composed" << std::endl;
+		//// 	std::string arg = std::move(args[0].getString());
+		//// 	std::vector<std::string> concat = std::move(args[2].getVector());
+		//// 	concat.push_back(std::move(arg));
+		//// 	return Value(std::move(concat));
+		//// })
 		;
 	_parser.rule("regexp_concat") //vector<shared_ptr<RegexpUnit>>
 		.production("regexp_repeat", [](auto&& args) -> Value {
-			// std::cout << "leaf repeat matched " << std::endl;
+			//// std::cout << "leaf repeat matched " << std::endl;
 			std::vector<std::string> output;
 			output.push_back(std::move(args[0].getString()));
 			return Value(std::move(output));
 		})
 		.production("regexp_concat", "regexp_repeat", [](auto&& args) -> Value {
-			// std::cout << "nonleaf repeat matched " << std::endl;
+			//// std::cout << "nonleaf repeat matched " << std::endl;
 			std::vector<std::string> output = std::move(args[0].getVector());
 			output.push_back(std::move(args[1].getString()));
 			return Value(std::move(output));
 		})
 		;
 	_parser.rule("regexp_repeat") //shared_ptr<RegexpUnit>
-		// .production("regexp_single", "REGEXP_PITER", "regexp_greedy", [](auto&& args) -> Value {
+		// .production("regexp_single", "REGEXP_PITER", /*"regexp_greedy",*/ [](auto&& args) -> Value {
 		// 	std::stringstream ss;
 		// 	ss << args[0].getString() << args[2].getString();
 		// 	return Value(std::move(ss.str()));
 		// })
 		.production("regexp_single", [](auto&& args) -> Value {
-			// std::cout << "matched single" << std::endl;
+			//// std::cout << "matched single" << std::endl;
 			return std::move(args[0]);
 		})
 		;
-	_parser.rule("regexp_greedy")
-		.production([](auto&&) -> Value { return {}; })
-		.production("REGEXP_OPTIONAL", [](auto&& args) -> Value { return std::move(args[0]); })
-		;
+	//// _parser.rule("regexp_greedy")
+	//// 	.production([](auto&&) -> Value { return {}; })
+	//// 	.production("REGEXP_OPTIONAL", [](auto&& args) -> Value { return std::move(args[0]); })
+	//// 	;
 
 	_parser.rule("regexp_single")
 		.production("LP", "regexp_or", "RP", [](auto&& args) -> Value { return std::move(args[0]); })
@@ -147,16 +148,11 @@ int main()
 
 
 
-
-
-
-
-
-
-
-
-
 	auto report = _parser.prepare();
+
+	pog::HtmlReport html(_parser);
+	html.save("html_index_commented.html");
+
 	if (!report)
 	{
 		fmt::print("{}\n", report.to_string());
